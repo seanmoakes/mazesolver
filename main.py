d266ed9e17dd51c5b2d8debc451120c44822081c
Sean Moakes <mail@seanmoakes.com>
2025-01-22 15:52:36 +0000

    Cell class added

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
-from graphics import Window, Line, Point
+from graphics import Window
+from cell import Cell
 
 
 def main():
     win = Window(800, 600)
-    point_A = Point(100, 100)
-    point_B = Point(150, 150)
-    point_C = Point(50, 150)
-    line_ab = Line(point_A, point_B)
-    line_ac = Line(point_A, point_C)
-    line_bc = Line(point_B, point_C)
-    line_color = "black"
-    win.draw_line(line_ab, line_color)
-    win.draw_line(line_ac, line_color)
-    win.draw_line(line_bc, line_color)
+
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+
     win.wait_for_close()
 
 
