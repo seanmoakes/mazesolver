9f0a7ca5ecbe01ea6fbec76dfab194e6cc9487de
Sean Moakes <mail@seanmoakes.com>
2025-01-22 16:24:36 +0000

    draw moving between cells

--- cell.py
+++ cell.py
@@ -14,6 +14,8 @@ class Cell:
         self._win = win
 
     def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
@@ -30,3 +32,20 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        fill_color = "gray" if undo else "red"
+
+        point_a = self.get_center_point()
+        point_b = to_cell.get_center_point()
+
+        line_ab = Line(point_a, point_b)
+        self._win.draw_line(line_ab, fill_color)
+
+    def get_center_point(self):
+        if self._x1 is None or self._x2 is None or self._y1 is None or self._y2 is None:
+            return
+
+        x = abs(self._x1 + self._x2) // 2
+        y = abs(self._y1 + self._y2) // 2
+        return Point(x, y)
