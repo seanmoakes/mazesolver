b3e84d0d7836cede69920edbd59b1890d3be3188
Sean Moakes <mail@seanmoakes.com>
2025-01-22 14:18:59 +0000

    window class added

--- graphics.py
+++ graphics.py
@@ -5,10 +5,9 @@ class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.__root.geometry(f"{width}x{height}")
-        self.canvas = Canvas()
-        self.canvas.pack()
-        self.running = False
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
@@ -16,17 +15,10 @@ class Window:
         self.__root.update()
 
     def wait_for_close(self):
-        self.running = True
-        while self.running:
+        self.__running = True
+        while self.__running:
             self.redraw()
+        print("window closed...")
 
     def close(self):
-        self.running = False
-
-
-def main():
-    win = Window(800, 600)
-    win.wait_for_close()
-
-
-main()
+        self.__running = False
