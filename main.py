fbc78da2f9b5486c48cc0963ac5c5e2afd46037c
Sean Moakes <mail@seanmoakes.com>
2025-01-22 21:53:53 +0000

    maze solving added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from graphics import Window
 from maze import Maze
+import sys
 
 
 def main():
@@ -10,9 +11,18 @@ def main():
     screen_y = 600
     cell_size_x = (screen_x - 2 * margin) / num_cols
     cell_size_y = (screen_y - 2 * margin) / num_rows
+
+    sys.setrecursionlimit(10000)
     win = Window(screen_x, screen_y)
 
     maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+    print("maze created")
+    is_solvable = maze.solve()
+
+    if not is_solvable:
+        print("maze cannot be solved!")
+    else:
+        print("maze solved!")
 
     win.wait_for_close()
 
